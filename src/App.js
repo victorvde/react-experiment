c9699132102ed050904f336e2532210abb555f3d
Victor van den Elzen <victor.vde@gmail.com>
2017-07-09 12:45:17 +0200

    reverse data flow

--- src/App.js
+++ src/App.js
@@ -1,30 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component, PureComponent } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-      return <div className="App">
-          <form>
-              <MMRInput>Current MMR:</MMRInput><br/>
-              <MMRInput>True MMR:</MMRInput></form>
-          <p>The difference is XXX</p>
-      </div>;
-  }
+function L(t, name) {
+    return {
+        value: t.state[name],
+        onChange: (v) => t.setState({[name]: v}),
+    };
 }
 
-class MMRInput extends Component {
+class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {value: 1};
+        this.state = {
+            current: 1000,
+            true: 3000,
+            goal: 2000,
+        };
     }
 
-    handleChange = (event) => { this.setState({value: event.target.value}); };
+    render() {
+        return <div className="App">
+            <form>
+                <MMRInput l={L(this, "current")}>Current MMR: </MMRInput><br/>
+                <MMRInput l={L(this, "true")}>True MMR: </MMRInput><br />
+                <MMRInput l={L(this, "goal")}>Goal MMR: </MMRInput>
+            </form>
+            <p>The values are {this.state.current} { this.state.true} {this.state.goal}.</p>
+        </div>;
+    }
+}
+
+class MMRInput extends PureComponent {
+    handleChange = (event) => { this.props.l.onChange(event.target.value); };
 
     render() {
         return <label>
             {this.props.children}
-            <input type="text" pattern="[0-9]+" size="4" value={this.state.value} onChange={this.handleChange} />
-            <input type="range" min="1" max="10000" value={this.state.value} onChange={this.handleChange} />
+            <input type="text" pattern="[0-9]+" size="4" value={this.props.l.value} onChange={this.handleChange} />
+            <input type="range" min="1" max="10000" value={this.props.l.value} onChange={this.handleChange} />
         </label>;
     }
 }

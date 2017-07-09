995f37043f3f519a0b9a5becf2fb91cfa0fe8d51
Victor van den Elzen <victor.vde@gmail.com>
2017-07-09 11:06:13 +0200

    start

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      return <div className="App">
+          <form>
+              <MMRInput>Current MMR:</MMRInput><br/>
+              <MMRInput>True MMR:</MMRInput></form>
+          <p>The difference is XXX</p>
+      </div>;
   }
+}
+
+class MMRInput extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: 1};
+    }
+
+    handleChange = (event) => { this.setState({value: event.target.value}); };
+
+    render() {
+        return <label>
+            {this.props.children}
+            <input type="text" pattern="[0-9]+" size="4" value={this.state.value} onChange={this.handleChange} />
+            <input type="range" min="1" max="10000" value={this.state.value} onChange={this.handleChange} />
+        </label>;
+    }
 }
 
 export default App;

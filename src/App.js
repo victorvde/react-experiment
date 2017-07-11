9c45f1a9632413299d30181d7e9acf63f26968c8
Victor van den Elzen <victor.vde@gmail.com>
2017-07-11 16:01:21 +0200

    polygon

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { Component, PureComponent } from 'react';
 import './App.css';
 
+function mmrData(current, real) {
+    return function(i) {
+        return [current + i*25, 0, real + i*25];
+    };
+}
+
 function L(t, name) {
     return {
         value: t.state[name],
@@ -23,8 +29,7 @@ class App extends Component {
                 <MMRInput {...L(this, "current")}>Current MMR: </MMRInput><br/>
                 <MMRInput {...L(this, "real")}>True MMR: </MMRInput><br />
             </form>
-            <p>The values are {this.state.current} { this.state.real}.</p>
-            <MMRGraph height={300} width={500} current={this.state.current} real={this.state.real} />
+            <MMRGraph height={500} width={500} f={mmrData(this.state.current, this.state.real)}/>
         </div>;
     }
 }
@@ -53,7 +58,7 @@ class MMRGraph extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            pos: [-10, 10-this.props.height],
+            pos: [0, -this.props.height],
             dragMouse: null,
             dragPos: null,
         };
@@ -76,8 +81,8 @@ class MMRGraph extends Component {
     updatePosition(e) {
         this.setState({
             pos: [
-                this.state.dragMouse[0] - e.screenX + this.state.dragPos[0],
-                this.state.dragMouse[1] - e.screenY + this.state.dragPos[1],
+                Math.max(0, this.state.dragMouse[0] - e.screenX + this.state.dragPos[0]),
+                Math.min(-this.props.height, this.state.dragMouse[1] - e.screenY + this.state.dragPos[1]),
             ],
         });
     }
@@ -105,16 +110,37 @@ class MMRGraph extends Component {
         let w = this.props.width;
         let h = this.props.height;
 
-        let max = 1000000;
-
         let [vx, vy] = this.state.pos;
 
-        let vlines = fixedSteps(vx, vx + w, 25);
-        let hlines = fixedSteps(vy, vy + h, 25);
-        let xmarks = fixedSteps(vx, vx + w, 100);
-        let ymarks = fixedSteps(vy, vy + h, 100);
+        let max = 1000000;
 
-        return <div><svg width={w} height={h} id="mmrgraph" onMouseDown={this.onMouseDown}>
+        let xscale = 1;
+        let xstep = 25;
+        let xmark_every = 5;
+        let xline_every = 5;
+        let yscale = 25;
+        let ystep = 1.25;
+        let ymark_every = 40;
+        let yline_every = 20;
+
+        let xlines = fixedSteps(vx, vx + w, xline_every * xstep);
+        let ylines = fixedSteps(vy, vy + h, yline_every * ystep);
+        let xmarks = fixedSteps(vx, vx + w, xmark_every * xstep);
+        let ymarks = fixedSteps(vy, vy + h, ymark_every * ystep);
+        let datax = fixedSteps(vx - xstep, vx + w + xstep, xstep);
+        let data = datax.map((x) => this.props.f(x / xstep));
+        let polygons = [];
+        for(let i=0; i < datax.length - 1; i++) {
+            let x1 = datax[i];
+            let x2 = datax[i+1];
+            let y1l = -data[i][0] / yscale * ystep;
+            let y1h = -data[i][2] / yscale * ystep;
+            let y2l = -data[i+1][0] / yscale * ystep;
+            let y2h = -data[i+1][2] / yscale * ystep;
+            polygons.push(`${x1},${y1l}  ${x1},${y1h} ${x2},${y2h} ${x2},${y2l}`);
+        }
+
+        return <svg width={w} height={h} id="mmrgraph" onMouseDown={this.onMouseDown}>
             <defs>
                 <filter id="solid">
                     <feFlood floodColor="#ffffff"/>
@@ -124,17 +150,17 @@ class MMRGraph extends Component {
             <rect x="0" y="0" width={w} height={h} fill="#f0f0ff" />
             <g transform={`translate(${-vx} ${-vy})`}>
                 <circle cx={0} cy={0} r="10" fill="#00ff00" />
-                {vlines.map((x, _) => <line x1={x+.5} x2={x+.5} y1={-max} y2={max} className={x===0?"axis":"grid"} key={`gv${x}`} />)}
-                {hlines.map((y, _) => <line x1={-max} x2={max} y1={y-.5} y2={y-.5} className={y===0?"axis":"grid"} key={`gh${y}`} />)}
+                {xlines.map((x) => <line x1={x+.5} x2={x+.5} y1={-max} y2={max} className={x===0?"axis":"grid"} key={`gv${x}`} />)}
+                {ylines.map((y) => <line x1={-max} x2={max} y1={y-.5} y2={y-.5} className={y===0?"axis":"grid"} key={`gh${y}`} />)}
+                {polygons.map((p) => <polygon points={p} fill="#880000" stroke="#880000" /> )}
             </g>
             <g transform={`translate(0 ${-vy})`}>
-                {ymarks.map((y, _) => <text filter="url(#solid)" x={0} y={y} dominantBaseline="central" key={`ym${y}`}>{-y}</text>)}
+                {ymarks.map((y) => <text filter="url(#solid)" x={0} y={y} dominantBaseline="text-before-edge" key={`ym${y}`}>{-y / ystep * yscale}</text>)}
             </g>
             <g transform={`translate(${-vx} 0)`}>
-                {xmarks.map((x, _) => <text filter="url(#solid)" x={x} y={h} textAnchor="middle" dominantBaseline="text-after-edge" key={`xm${x}`}>{x}</text>)}
+                {xmarks.map((x) => <text filter="url(#solid)" x={x} y={h} textAnchor="start" dominantBaseline="text-after-edge" key={`xm${x}`}>{x / xstep * xscale}</text>)}
             </g>
-        </svg>
-        <p>{this.state.pos[0]} {this.state.pos[1]}</p></div>;
+        </svg>;
     }
 }
 
